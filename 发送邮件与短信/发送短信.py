5a7717e143210347d9c80cbe02cfb67b757ee76a
Yu_dezhi <yudezhi.cn@gmail.com>
2019-07-29 21:22:43 +0800

    发送邮件与短信

---
+++ 发送邮件与短信/发送短信.py
@@ -0,0 +1,23 @@
+import urllib.parse
+import http.client
+import json
+
+
+def main():
+    host  = "106.ihuyi.com"
+    sms_send_uri = "/webservice/sms.php?method=Submit"
+    # 下面的参数需要填入自己注册的账号和对应的密码
+    params = urllib.parse.urlencode({'account': 'C82959965', 'password' : '5e8682880507fbe184c47d45ac8e1643', 'content': '您的验证码是：13243545。请不要把验证码泄露给其他人。', 'mobile': '18800115305', 'format':'json' })
+    print(params)
+    headers = {'Content-type': 'application/x-www-form-urlencoded', 'Accept': 'text/plain'}
+    conn = http.client.HTTPConnection(host, port=80, timeout=30)
+    conn.request('POST', sms_send_uri, params, headers)
+    response = conn.getresponse()
+    response_str = response.read()
+    jsonstr = response_str.decode('utf-8')
+    print(json.loads(jsonstr))
+    conn.close()
+
+
+if __name__ == '__main__':
+    main()
